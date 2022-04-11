3d3ca5385a5377ba42fadc72e8ea5abac7696ccf
Christina <ginihumer96@yahoo.de>
2022-04-11 08:38:32 +0200

    update design

--- node_modules/vue-cli-plugin-vuetify/generator/templates/v3/vite/vite.config.js
+++ node_modules/vue-cli-plugin-vuetify/generator/templates/v3/vite/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from '@vuetify/vite-plugin'
 
-import path from 'path'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
